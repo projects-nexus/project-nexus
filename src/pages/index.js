de25ccd1903a82d1d12dcf53db161054cc4b992b
itsmethekid <pr4j4p4t1.hr1d4y4@gmail.com>
2022-01-29 08:11:02 -0800

    project-nexus: miatoll: Nexus Kernel X3 * also from now on the md5 checksum will be added only if the maintainers give it.

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/notes/lavender/latest">
+            to="/docs/notes/miatoll/latest">
             Download 
           </Link>
         </div>
